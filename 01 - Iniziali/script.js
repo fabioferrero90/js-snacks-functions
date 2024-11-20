48af298b9116a6f1eabfe3829caa5c57ca85b6f7
fabioferrero90 <fabio.ff90@gmail.com>
2024-11-20 13:23:36 +0100

    feat: first exercise done

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,27 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let initials = [];
+let initialsRaw = [];
 
 // Dichiara la funzione qui.
+const getInitials = array => {
+  for (const element of array) {
+    initials.push(element.charAt(0))
+  }
+}
+
+function getInitialsRaw(array) {
+  for (const element of array) {
+    initialsRaw.push(element.charAt(0))
+  }
+  return initialsRaw
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+getInitials(names)
 
-
-
+console.log(initials)
+console.log(getInitialsRaw(names))
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
